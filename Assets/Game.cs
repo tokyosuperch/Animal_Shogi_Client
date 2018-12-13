9b8c5b3d6f4750306b16e14ace2c9517bd203557
東京スーパーチャンネル <tokyosuperchannel@gmail.com>
2018-12-13 19:51:16 +0900

    どうぶつしょうぎサーバーへの接続および15秒おきのKeep-Aliveを実装

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+using System.Net;
 
 public class Game : MonoBehaviour {
     GameObject[,] masume = new GameObject[4, 3];
@@ -23,16 +29,104 @@ public class Game : MonoBehaviour {
     public Sprite himage;
     public Sprite nimage;
     GameObject destroyed;
+    bool? iamsente = null;
+    float timeElapsed;
+    // メッセージを管理するリスト
+    private List<string> messages = new List<string>();
+    private string currentMessage = string.Empty;
+    // Server
+    NetworkStream stream = null;
+    bool isStopReading = false;
+    byte[] readbuf;
 
     // Use this for initialization
-    void Start() {
+    private IEnumerator Start() {
         masuinit();
         komainit();
+        keepaliver();
+        Debug.Log("START START");
+        readbuf = new byte[1024];
+        while (true) {
+            if (!isStopReading) { StartCoroutine(ReadMessage()); }
+            yield return null;
+        }
+    }
+    private IEnumerator SendMessage(string message) {
+        Debug.Log("START SendMessage:" + message);
+
+        if (stream == null) {
+            stream = GetNetworkStream();
+        }
+        //サーバーにデータを送信する
+        Encoding enc = Encoding.UTF8;
+        byte[] sendBytes = enc.GetBytes(message + "\n");
+        //データを送信する
+        stream.Write(sendBytes, 0, sendBytes.Length);
+        yield break;
+    }
+
+    private IEnumerator ReadMessage() {
+        stream = GetNetworkStream();
+        // 非同期で待ち受けする
+        stream.BeginRead(readbuf, 0, readbuf.Length, new AsyncCallback(ReadCallback), null);
+        isStopReading = false;
+        yield return null;
+    }
+
+    public void ReadCallback(IAsyncResult ar) {
+        Encoding enc = Encoding.UTF8;
+        stream = GetNetworkStream();
+        int bytes = stream.EndRead(ar);
+        string message = enc.GetString(readbuf, 0, bytes);
+        // message = message.Replace("\r", "").Replace("\n", "");
+        isStopReading = false;
+        messages.Add(message);
+        Debug.Log(message);
+        if (message.IndexOf("BEGIN Game_Summary") != -1 || message.IndexOf("Game_ID") != -1) playerinit(message);
+    }
+
+    private NetworkStream GetNetworkStream() {
+        if (stream != null && stream.CanRead) {
+            return stream;
+        }
+
+        string ipOrHost = "shogi.keio.app";
+        int port = 80;
+
+        //TcpClientを作成し、サーバーと接続する
+        TcpClient tcp = new TcpClient(ipOrHost, port);
+        Debug.Log("success conn server");
+
+        //NetworkStreamを取得する
+        return tcp.GetStream();
+    }
+    
+    private void keepaliver() {
+        StartCoroutine(KeepAlive());
+    }
+
+    private IEnumerator KeepAlive() {
+        while (true) {
+            StartCoroutine(SendMessage("Keep-Alive"));
+            yield return new WaitForSeconds(15);
+        }
     }
 
     // Update is called once per frame
     void Update() {
 
+    }
+
+    public void playerinit(string message) {
+        if (message.IndexOf("Your_Turn:") != -1) {
+            if (message.Substring(message.IndexOf("Your_Turn:") + 10, 1) == "+") {
+                Debug.Log("先手");
+                iamsente = true;
+            } else if (message.Substring(message.IndexOf("Your_Turn:") + 10, 1) == "-") {
+                Debug.Log("後手");
+                iamsente = false;
+            }
+        }
     }
 
     public void masuinit() {
@@ -70,6 +164,7 @@ public class Game : MonoBehaviour {
     }
 
     public void komaclick(string komaname) {
+        StartCoroutine(SendMessage("LOGIN:supachan_Client(Debug)"));
         destroyed = null;
         GameObject temporary = null;
         if (komaname.StartsWith("h", false, null)) {
@@ -78,10 +173,11 @@ public class Game : MonoBehaviour {
             temporary = zou[int.Parse(komaname.Substring(1, 1))];
         } else if (komaname.StartsWith("k", false, null)) {
             temporary = kirin[int.Parse(komaname.Substring(1, 1))];
-        } else if (komaname.StartsWith("l", false, null)){
+        } else if (komaname.StartsWith("l", false, null)) {
             temporary = lion[int.Parse(komaname.Substring(1, 1))];
         }
-        if (senteturn) {
+        if (iamsente == null) {
+        } else if (senteturn && iamsente == true) {
             char[] initial = temporary.name.Substring(0, 1).ToCharArray();
             if (char.IsLower(initial[0])) {
                 moving = temporary;
@@ -97,7 +193,7 @@ public class Game : MonoBehaviour {
                 destroyed = temporary;
                 masuclick(destroyed.name.Substring(1, 1) + destroyed.name.Substring(2, 1));
             }
-        } else {
+        } else if (!senteturn && iamsente == false) {
             char[] initial = temporary.name.Substring(0, 1).ToCharArray();
             if (char.IsUpper(initial[0])) {
                 moving = temporary;
